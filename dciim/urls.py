0c4ae5e8de43fc73013f598452521f06e08ca5ae
Mohammad Rafiee <m.rafieee@gmail.com>
2015-06-09 16:30:43 +0430

    change admin url to root

--- dciim/urls.py
+++ dciim/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     # url(r'^$', 'dciim.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(admin.site.urls)),
 ]
